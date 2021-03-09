64bf7e2e62d69315e4c64201c2b81da2d727f049
nrtkn <38586514+nrtkn@users.noreply.github.com>
2021-03-09 17:33:21 +0300

    Add files via upload

---
+++ EntityFrameWorkDemo/ProductDal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkDemo
+{
+    public class ProductDal
+    {
+        public  List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void Add(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                //context.Products.Add(product);
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+        public void Update(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
